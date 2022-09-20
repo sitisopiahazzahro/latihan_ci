067625306e3e41f000c3a8e8a94b257f32db6835
sitisopiahazzahro <sitisopiahazzahro1711@.com>
2022-09-20 16:55:01 +0700

    first commit

---
+++ application/views/tampil.php
@@ -0,0 +1,10 @@
+<html>
+<head>
+<title>Latihan Pertama</title>
+</head>
+<body>
+	<p>Latihan Pertama saya belajar framework codeigniter</p>
+	<p>Framework Codeigniter sangat mudah dipelajari</p>
+	
+</body>
+</html>
